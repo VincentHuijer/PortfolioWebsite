14bf1d58b5240e0c7cd2606aed33e57f7e8fe379
Helloguis <94686622+VincentHuijer@users.noreply.github.com>
2024-02-02 21:18:30 +0100

    WIP frontpage

--- src/views/Profiel.js
+++ src/views/Profiel.js
@@ -1,42 +1,32 @@
-// import React from 'react';
-// import VincentHuijer from '../media/VincentHuijer.png';
-// import Sidebar from '../components/Sidebar';
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'; 
+import { faM } from '@fortawesome/free-solid-svg-icons';
 
 const Profiel = () => {
   return (
-    <div className="">
-      {/* <div className="bg-[#f4f4f4] p-3 flex items-center justify-center mb-7">
-        <div className="text-black text-2xl font-serif">Vincent Huijer</div>
-      </div>
-      <div>
-        <div className='flex justify-center'>
-          <div className='w-8/12 ml-36'>
-            <div>
-              <img src={VincentHuijer} alt="Vincent Huijer" className="w-3/4" />
-            </div>
-            <div className="pl-64 text-sm">Vincent Huijer</div>
-
-            <div>
-              <div className="font-serif text-lg">Studie</div>
-              <div className='mb-5'>Het begin van mijn onderwijsreis vond plaats op Stanislas in de mavo. Na mijn tijd op de mavo maakte ik de overstap naar de havo, waar ik mijn academische horizon verder verbreedde. Mijn studiepad bracht me uiteindelijk naar de Haagse Hogeschool, waar ik aanvankelijk HBO Rechten studeerde. Na een zorgvuldige afweging besloot ik echter dat deze richting niet volledig aan mijn verwachtingen voldeed. Daarom maakte ik de overstap naar HBO ICT, waar mijn passie en interesse beter tot hun recht kwamen. Deze beslissing heeft mijn onderwijsreis verrijkt en mijn pad geleid naar een vakgebied dat beter aansluit bij mijn persoonlijke en professionele doelen.</div>
-            </div>
-
-            <div>
-              <div className="font-serif text-lg">Gym</div>
-              <div className='mb-5'>Sporten is voor mij niet alleen een fysieke activiteit, maar een essentieel onderdeel van mijn levensstijl en welzijn. Er zijn talloze redenen waarom ik sporten als iets belangrijks beschouw, en het gaat verder dan alleen het streven naar een gezond lichaam. Allereerst ervaar ik sporten als een krachtig middel om stress te verminderen en mijn mentale gezondheid te verbeteren. Tijdens het sporten kan ik mijn gedachten loslaten en me concentreren op het moment, wat resulteert in een gevoel van ontspanning en helderheid. Het helpt me om spanningen kwijt te raken en even te ontsnappen aan de dagelijkse beslommeringen.</div>
-            </div>
-
-          </div>
-
-
-
-          <div className="w-4/12 p-4 mr-36">
-            <Sidebar/>
+    <div className="bg-[#1D1D20]">
+      <div className="flex flex-col items-start justify-center content-center h-screen text-gray-400 mx-96 ">
+        <div>
+          <div className='text-gray-400'>
+            <FontAwesomeIcon icon={faM} size="2x" className='mr-2'/>
+            <FontAwesomeIcon icon={faGithub} size="2x" className='mr-2'/>
+            <FontAwesomeIcon icon={faLinkedin} size="2x" className='mr-2'/>
           </div>
         </div>
-      </div> */}
+        <div className='font-sans text-9xl'>
+          Welkom
+        </div>
+        <div className='relative'>
+          <div className='border-l-3 p-4 ml-2'>
+            Hoi, mijn naam is Vincent Huijer. Ik ben 24 jaar oud en ik woon in Nootdorp, Zuid-Holland. Op dit moment volg ik een opleiding ICT Software Engineering aan De Haagse Hogeschool. Ik ben al van jongs af aan geïnteresseerd in computers en heb verschillende activiteiten gedaan, zoals videobewerking, spelen met 3D-modellen in Blender en gamen, wat uiteindelijk leidde tot mijn interesse in programmeren. Tijdens mijn middelbare school begon ik naar alle mogelijkheden binnen de IT-sector te kijken. Hier ontdekte ik softwareontwikkeling en ben ik er dieper in gedoken. Sindsdien breid ik elke week mijn kennis over webontwikkeling uit en heb ik aan verschillende projecten gewerkt.
+          </div>
+          <div className='absolute top-0 left-0 bottom-0 w-1 bg-gradient-to-b from-[#87378D] to-[#877E4C]'></div>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Profiel;
+
