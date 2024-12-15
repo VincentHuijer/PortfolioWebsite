cc672db491e8a19e4b716bcf74bbae7e36a96b6a
Helloguis <94686622+VincentHuijer@users.noreply.github.com>
2024-12-15 22:54:22 +0100

    Update Todo.js

--- src/views/Todo.js
+++ src/views/Todo.js
@@ -1,22 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Todo = () => {
-  const [ test, setTest] = useState("test not yet started");
+  const [test, setTest] = useState("test not yet started");
+  const [effect, setEffect] = useState("Effect incomplete");
 
   function updateTest(value) {
     setTest(value);
   }
 
+  useEffect(() => {
+    setEffect("effect complete")
+
+  }, []);
+
   return (
     <div>
-      <div onClick={() => {updateTest("test has been updated")}}>
-      click to update test
+      <div
+        onClick={() => {
+          updateTest("test has been updated");
+        }}
+      >
+        click to update test
       </div>
-      <div>
-        { test }
-      </div>
+      <div>{test}</div>
+      <div> {effect} </div>
     </div>
-  )
+  );
 };
 
 export default Todo;
